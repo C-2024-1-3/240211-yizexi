2f237ce6d526599d927e05a7fe24d33e0bff5d7c
天才 <@>
2024-11-07 21:15:10 +0800

    45

---
+++ c++第一次实验报告2402易泽西.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include<iomanip>
+using namespace std;
+//实验报告
+int main()
+{// task 1:
+	int k=0;
+	int i = k + 1;
+	cout << i++ << endl;
+	 i = 1;
+	 cout << i++ << endl;
+	cout << "welcome to c++" << endl;
+	system("pause");
+	// task 2:
+	int ban, gao;
+	cin >> ban>> gao;
+	double pai = 3.14;
+	cout << pai * ban * ban * gao / 3 << endl;
+	system("pause");
+	// task 3:
+	cout << "char length" << sizeof(char) << endl;
+	cout << "int length" << sizeof(int) << endl;
+	system("pause");
+	// task 4:
+	unsigned int testUnint = 65534;
+	cout << "output in unsigned int 1 type:" << testUnint << endl;
+	cout << "output in char type:!" << static_cast<char>(testUnint)<< endl;
+	cout << "output in short type:" << static_cast<short>(testUnint) << endl;
+	cout << "output in  int  type:" << static_cast<int>(testUnint) << endl;
+	cout << "output in double type:" << static_cast<double>(testUnint) << endl;
+	cout << "output in double type:" <<setprecision(4)<< static_cast<double>(testUnint) << endl;
+	cout << "output in Hex unsigned int  type:" << hex<<testUnint << endl;
+	cout << "output in oct unsigned int  type:" << oct << testUnint << endl;
+
+	system("pause");
+	// task 5:
+	double hua;
+	cin >> hua;
+	double she = (hua - 32) / 1.8;
+	cout << she;
+	system("pause");
+    return 0;
+}
+
---
+++ c++第二次实验报告240211易泽西.cpp
@@ -0,0 +1,160 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+cout << "task 1:" << endl;
+	char a;
+	cin >> a;
+	if (a >= 'a' && a <= 'z') {
+		a = a - 32;
+		cout << a << endl;
+	}
+	else {
+		cout << a + 1 << endl;
+	}
+	system("pause");
+	cout << "task 2:" << endl;
+	double x = 0;
+	double y = 0;
+	cin >> x;
+	if (0 < x && x < 1) {
+		y = 3 - 2 * x;
+	}
+	else if (1 <= x && x < 5) {
+		y = 2 / (4 * x) + 1;
+	}
+	else if (5 <= x && x < 10) {
+		y = x * x;
+	}
+	cout << y << endl;
+	system("pause");
+	cout << "task 3:" << endl;
+	double j, k, l;
+	cin >> j >> k >> l;
+	if (j + k <= l || j + l <= k || k + l <= j) {
+		cout << "不能构成三角形" << endl;
+	}
+	else {
+		double zhouchang = j + k + l;
+		if (j == k || j == l || k == l) {
+			cout << "是等腰三角形" << endl;
+		}
+		cout << "周长为：" << zhouchang;
+		system("pause");
+	}
+	cout << "task 4:" << endl;
+	double num1, num2;
+	char yun;
+	double guo = 0;
+	cout << "请分别输入第一，二个数字：" << endl;
+	cin >> num1>>num2 ;
+	cout << "请输入运算符" << endl;
+	cin >> yun;
+	if (yun == '+') {
+		guo = num1 + num2;
+	}
+	else if (yun == '-') {
+		guo = num1 - num2;
+	}
+	else if (yun == '*') {
+		guo = num1 * num2;
+	}
+	else if (yun == '/') {
+		if (num2 == 0) {
+			cout << "除数不能为0" << endl;
+		}
+		else {
+			guo = num1 / num2;
+		}
+	}
+	else {
+		cout << "请输入合法的操作符" << endl;
+	}
+	cout << "结果为：" << guo << endl;
+	system("pause");
+    cout << "task 5:" << endl;
+	int word = 0, space = 0, number = 0, other = 0;
+	char c;
+	cout << "请输入：" << endl;
+	c = getchar();
+	while (c != '\n') {
+		if ((c >= 'a' && c <= 'z') || c >= 'A' && c <= 'Z') {
+			word++;
+		}
+		else if (c == ' ') {
+			space++;
+		}
+		else if (c >= '0' && c <= '9') {
+			number++;
+		}
+		else {
+			other++;
+		}
+		c = getchar();
+	}
+	cout << "英文字母有" << word << "个" << endl;
+	cout << "数字有" << number << "个" << endl;
+	cout << "空格有" << space << "个" << endl;
+	cout << "其他字符有" << other << "个" << endl;
+	system("pause");
+	cout << "task 6:" << endl;
+	int a1, a2;
+	cin >> a1 >> a2;
+	int a3 = 1;
+	int a4 = 1;
+	int yue = 1;
+	int bei = 1;
+	for (; a3 <= a1 && a3 <= a2; a3++) {
+		if (a1 % a3 == 0 && a2 % a3 == 0) {
+			yue = a3;
+		}
+	}
+	for (;; a4++) {
+		if (a4 % a1 == 0 && a4 % a2 == 0) {
+			bei = a4;
+			break;
+		}
+	}
+	cout << "最大公约数为：" << yue << "最小公倍数为：" << bei;
+	system("pause");
+	cout << "task 7:" << endl;
+	for (int xing1 = 1; xing1 < 6; xing1++) {
+		for (int xing2 = 1; xing2 <= xing1; xing2++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	cout << "task 8:" << endl;
+	double u = 0; cout << "请输入所要开根的数字：" << endl;
+	cin >> u;
+	if (u >= 0) {
+		double x0 = u;
+		double x1 = (x0 + u / x0) / 2;
+		for (; 10000 * (x0 - x1) >= 0.00001 || 10000 * (x1 - x0) >= 0.00001;) {
+			x0 = x1;
+			x1 = (x0 + u / x0) / 2;
+		}
+		cout << "结果为：" << x1;
+	}
+	else {
+		cout << "请不要输入负数" << endl;
+		system("pause");
+		cout << "task 9:" << endl;
+		double ping = 0.8;
+		int tian = 0;
+		int shu = 2;
+		int zong = 0;
+		for (; shu <= 100; ) {
+			zong += shu;
+			shu = 2 * shu;
+			tian++;
+		}
+		double jun = ping * zong / tian;
+		cout << jun;
+		system("pause");
+		return 0;
+
+	}
+}
+
+
